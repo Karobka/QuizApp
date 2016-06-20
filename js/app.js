e8e4358d4ea16899698e6d016b6f2e4f9bf7f645
Tim Wright <mithgirw@gmail.com>
2016-06-20 10:53:36 -0600

    added questions array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 $(document).ready(function() {
     $("#clicktobegin").css("display", "inline-block");
     $("#clicktobegin").on("click", function(event){
@@ -11,4 +11,52 @@ $(document).ready(function() {
         $("button").css("display", "inline-block");
         $(".instructions").css("sidplay", "inline-block");
     });
+
+    var questionsArray = [
+        {
+            question: "Question #1: In which state is the Sequoia National Park?",
+            choicesArray: ["Utah", "Arkansas", "California", "Idaho"],
+            correctAnswer: "California"
+        },
+        {
+            question: "Question #2: In which state is the Arches National Park?",
+            choicesArray: ["Nevada", "Utah", "Arkansas", "California"],
+            correctAnswer: "Utah"
+        },
+        {
+            question: "Question #3: In which state is the Haleakala National Park?",
+            choicesArray: ["Hawaii", "Texas", "Washington", "Montana"],
+            correctAnswer: "Hawaii"
+        },
+        {
+            question: "Question #5: In which state is the Hot Springs National Park?",
+            choicesArray: ["South Dakota", "California", "Montana", "Arkansas"],
+            correctAnswer: "Arkansas"
+        },
+        {
+            question: "Question #6: In which state is the Mount Rainier National Park?",
+            choicesArray: ["Maine", "Washington", "Idaho", "South Dakota"],
+            correctAnswer: "Washinton"
+        },
+        {
+            question: "Question #7: In which state is the Bryce Canyon National Park?",
+            choicesArray: ["Montana", "Maine", "California", "Utah"],
+            correctAnswer: "Utah"
+        },
+        {
+            question: "Question #8: In which state is the Joshua Tree National Park?",
+            choicesArray: ["California", "Nevada", "Idaho", "Hawaii"],
+            correctAnswer: "California"
+        },
+        {
+            question: "Question #9: In which state is the Acadia National Park?",
+            choicesArray: ["Florida", "Idaho", "Maine", "Arizona"],
+            correctAnswer: "Maine"
+        },
+        {
+            question: "Question #10: In which state is the Glacier National Park?",
+            choicesArray: ["Alaska", "Montana", "Oregon", "South Dakota"],
+            correctAnswer: "Montana"
+        }
+    ]
 });
